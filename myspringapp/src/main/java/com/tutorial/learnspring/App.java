978ffd7fcbbb23ee93af29fd72d5be1bcdc449a9
raghavanss <ssraghavan@gmail.com>
2016-08-13 20:36:34 -0500

    First Project using Spring bean

--- myspringapp/src/main/java/com/tutorial/learnspring/App.java
+++ myspringapp/src/main/java/com/tutorial/learnspring/App.java
@@ -1,13 +1,34 @@
 package com.tutorial.learnspring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	/* public static String getClasspathString() {
+	     StringBuffer classpath = new StringBuffer();
+	     ClassLoader applicationClassLoader = App.class.getClassLoader();
+	     if (applicationClassLoader == null) {
+	         applicationClassLoader = ClassLoader.getSystemClassLoader();
+	     }
+	     URL[] urls = ((URLClassLoader)applicationClassLoader).getURLs();
+	      for(int i=0; i < urls.length; i++) {
+	          classpath.append(urls[i].getFile()).append("\r\n");
+	      }    
+	     
+	      return classpath.toString();
+	  }*/
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("LearnSpringBean.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+	
+	
+	}
 }
---
+++ myspringapp/src/main/java/com/tutorial/learnspring/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.tutorial.learnspring;
+
+public class HelloWorld {
+
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+}
